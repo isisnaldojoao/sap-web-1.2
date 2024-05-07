f1e9c494608bde970b551f5998921dd49e43ee92
Gabriel-Gomes-Amorim <gomez.amorim18@hotmail.com>
2024-05-06 22:09:49 -0300

    feat: implementing home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,21 @@
+import { CardApps } from "../../components/CardApps";
 import { Sidebar } from "../../components/Sidebar";
+import { Container, ContainerCard, SubTitle, TitleMain } from "./styles";
 
 export const Home = () => {
-  return <Sidebar />;
+  return (
+    <Container>
+      <Sidebar />
+      <TitleMain>Boas vindas! 👋</TitleMain>
+      <SubTitle>Baixe os Apps</SubTitle>
+
+      <ContainerCard>
+        <CardApps
+          icon="src/assets/icons/icon-coletor.svg"
+          textMain="Aplicação Coletor Offline"
+          textSecondary="BAIXAR"
+        />
+      </ContainerCard>
+    </Container>
+  );
 };
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #f5f5f5;
+  font-family: "Poppins";
+  color: #344054;
+  height: 100vh;
+`;
+
+export const TitleMain = styled.h1`
+  width: 260px;
+  height: 48px;
+  font-weight: 600;
+  margin-top: 88px;
+  left: 390px;
+  position: absolute;
+  top: 0;
+  color: #344054;
+  font-family: "Poppins";
+`;
+
+export const SubTitle = styled.h3`
+  width: 141px;
+  height: 30px;
+  font-weight: 600;
+  margin-top: 181px;
+  left: 390px;
+  position: absolute;
+  top: 0;
+  color: #344054;
+  font-family: "Poppins";
+`;
+
+export const ContainerCard = styled.div`
+  margin-top: 239px;
+  margin-left: 390px;
+  position: absolute;
+  border-radius: 15px;
+  top: 0;
+`;
