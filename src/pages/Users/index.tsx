842334add67ffdd97fcefe789f193e6ebeb1dfd2
prodsistem <suporte@prodsistem.com.br>
2024-06-06 09:18:30 -0300

    ajustes e correoces na pagina de usuarios

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,22 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TableUsers } from "../../components/TableUsers";
 import { Container, Title } from "./styles";
+import axios from "axios";
 
-type AccessLevel = 'admin' | 'operator' | 'supervisor';
-type Status = 'active' | 'inactive';
+//type AccessLevel = 'admin' | 'operator' | 'supervisor';
+type Status = 'ativo' | 'inativo';
 
-export interface User {
-  id: string;
-  name: string;
-  user: string;
-  email: string | null;
-  accessLevel: AccessLevel;
-  status: Status;
-  lastLogin: Date;
+export interface NivelDeAcesso {
+  codigo: string;
+  nome: string;
+  nivel: number;
+  status: string;
 }
 
-const data: User[] = [
-  { id: '1', name: 'Marcos Teixeira', user: 'marcos', email: 'marcos@usuario.com', accessLevel: 'operator', status: 'active', lastLogin: new Date('2023-05-01T10:20:30Z') },
+export interface User {
+  codigo: string;
+  nome: string;
+  nomeDeUsuario: string;
+  email: string | null;
+  niveisDeAcesso: NivelDeAcesso;
+  status: Status;
+  ultimoLogin: Date;
+}
+
+/*const data: User[] = [
+  { id: '1', name: 'Marcos Teixeira', user: 'marcos', email: 'marcos@usuario.com', NivelDeAcesso: 'operator', status: 'active', lastLogin: new Date('2023-05-01T10:20:30Z') },
   { id: '2', name: 'Administrador', user: 'Administrador', email: null, accessLevel: 'admin', status: 'active', lastLogin: new Date('2023-04-12T08:15:45Z') },
   { id: '3', name: 'João Pereira', user: 'joao', email: 'joao@usuario.com', accessLevel: 'supervisor', status: 'active', lastLogin: new Date('2023-03-23T12:45:00Z') },
   { id: '4', name: 'Maria Souza', user: 'maria', email: 'maria@usuario.com', accessLevel: 'operator', status: 'active', lastLogin: new Date('2023-02-14T07:30:20Z') },
@@ -24,17 +32,44 @@ const data: User[] = [
   { id: '7', name: 'Pedro Santos', user: 'pedro', email: 'pedro@usuario.com', accessLevel: 'supervisor', status: 'active', lastLogin: new Date('2022-11-14T04:20:30Z') },
   { id: '9', name: 'Ricardo Mendes', user: 'ricardo', email: 'ricardo@usuario.com', accessLevel: 'operator', status: 'active', lastLogin: new Date('2022-09-22T06:45:00Z') },
   { id: '10', name: 'Juliana Costa', user: 'juliana', email: 'juliana@usuario.com', accessLevel: 'supervisor', status: 'inactive', lastLogin: new Date('2022-08-11T10:30:20Z') }
-];
+];*/
 
 export const Users = () => {
-  const [users, setUsers] = useState<User[]>(data);
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get<User[]>('http://localhost:3000/usuarios');
+
+        console.log(response.data);
+
+        const usersData = response.data.map((user) => ({
+          ...user,
+        }));
+        setUsers(usersData);
+        console.log(usersData);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
   
   function handleActivateUser(userId: string) {
-    setUsers((prevState) => prevState.map((user) => user.id === userId ? { ...user, status: 'active' } : user));
+    setUsers((prevState) => prevState.map((user) => user.codigo === userId ? { ...user, status: 'ativo' } : user));
   }
   
   function handleDeactivateUser(userId: string) {
-    setUsers((prevState) => prevState.map((user) => user.id === userId ? { ...user, status: 'inactive' } : user));
+    setUsers((prevState) => prevState.map((user) => user.codigo === userId ? { ...user, status: 'inativo' } : user));
+  }
+
+  if (loading) {
+    return <div>Carregando...</div>;
   }
 
   return (
