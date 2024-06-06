339b7e30eeec64899ae8d43cefc66714db3dbd5b
prodsistem <suporte@prodsistem.com.br>
2024-06-06 09:16:58 -0300

    ajustada porta para não conflitar com a porta do backend somente no ambiente de desenvolvimento

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 3000,
+    port: 4000,
     strictPort: true,
    },
   server: {
-    port: 3000,
+    port: 4000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3000/",
+    origin: "http://0.0.0.0:4000/",
    },
 })
